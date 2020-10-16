80183d1a774208b1dd5cace863ca6268857a4adf
Abhijit Ghatnekar <aghatnekar@qualys.com>
2020-10-16 21:57:51 +0530

    Added method for deleting element in an LinkedList

--- LinkedList/single_list.py
+++ LinkedList/single_list.py
@@ -1,37 +1,95 @@
 
 class SingleLinkedList:
+
+    nodes = None
+
     class Node:
         def __init__(self, data):
             self.data = data
             self.next = None
 
-
-    def append(self, data, node=None):
-        temp_node = SingleLinkedList.Node(data)
-        if node is None:
-            node = temp_node
+    def prepend(self, data):
+        """
+        O(1) Time/Space.
+            1. Time: We are prepending an element to the front of the linkedlist so O(1).
+            2. Space: We are just allocating space for 1 node so O(1)
+        :param data:
+        :return:
+        """
+        node = SingleLinkedList.Node(data)
+        if self.nodes is None:
+            self.nodes = node
         else:
-            temp_node2 = node
-            while temp_node2.next is not None:
-                temp_node2 = temp_node2.next
-            temp_node2.next = temp_node
+            current_first_node = self.nodes
+            node.next = current_first_node
+            self.nodes = node
+
+    def append(self, data):
+        """
+        O(N) Time, O(1) Space
+        1. Time: We are iterating through N-1 nodes to get to the Nth node to append data after that node.
+        2. Space: We are just constructing one node. However, if we are creating a list within for-loop, this will also be O(N)
+        :param data:
+        :return:
+        """
+        node = SingleLinkedList.Node(data)
+        if self.nodes is None:
+            self.nodes = node
+        else:
+            temp_node = self.nodes
+            while temp_node.next is not None:
+                temp_node = temp_node.next
+            temp_node.next = node
         return node
 
-    def traverse(self, node):
+    def traverse(self):
+        """
+        O(N) Time, O(1) Space
+        1. Time: We iterate through N elements hence O(N)
+        2. Space: We are'nt allocating any new memory.
+        :return:
+        """
         node_list = ""
-        while node:
-            node_list += str(node.data) + "-->"
-            if node.next is None:
+        while self.nodes:
+            node_list += str(self.nodes.data) + "-->"
+            if self.nodes.next is None:
                 node_list += "NULL"
-            node = node.next
+            self.nodes = self.nodes.next
 
         return node_list
 
+    def delete_data(self, data):
+        """
+        1. Time Complexity: O(N)
+          --> We do N lookups to find the data to delete
+        2. Space Complexity: O(1)
+          --> We deallocate just one node.
 
-if __name__=='__main__':
+        :param data:
+        :return:
+        """
+        if self.nodes.data == data:
+            self.nodes = self.nodes.next
+            return True
+        temp_node = self.nodes
+        prev_node = None
+        while temp_node:
+            if temp_node.data == data:
+                break
+            prev_node = temp_node
+            temp_node = temp_node.next
+        if temp_node is not None:
+            prev_node.next = temp_node.next
+            temp_node = None
+            return True
+        return False
+
+
+if __name__ == '__main__':
     A = [1, 2, 3, 4, 5]
     slist = SingleLinkedList()
-    node = None
     for loop_ctr in range(0, len(A)):
-        node = slist.append(A[loop_ctr], node)
-    slist.traverse(node)
+        slist.append(A[loop_ctr])
+    slist.prepend(3)
+    print(slist.delete_data(3))
+    print(slist.traverse())
