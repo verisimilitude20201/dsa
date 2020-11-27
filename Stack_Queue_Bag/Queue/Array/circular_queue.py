2cd0a69c9987c0733a2e25b7761a21b8f6e7987c
Abhijit Ghatnekar <abhighat@yahoo.co.in>
2020-11-27 22:39:42 +0530

    Restructured the folders

--- Stack_Queue_Bag/Queue/Array/circular_queue.py
+++ Stack_Queue_Bag/Queue/Array/circular_queue.py
@@ -1,3 +1,30 @@
+"""
+Approach:
+--------
+Using front and rear pointers we will treat an array as a circular array and wound back front and rear pointers whenever they get past the end of the array
+
+Main conditions
+
+1. Enqueue
+    a. front == rear == -1. Array is empty. Reassign both of them to 0 and insert element at rear.
+    b. If 1.a is not true, Increment rear by (rear + 1) % length of array and reassign element at rear
+    c. Check if (rear + 1) % length of array is equal to front. If yes, the array is full.
+
+2. Dequeue:
+    a. Check if front == rear. If so only one element is left and so get it, and set both front and rear to -1 before returning it.
+    b. Access the data in array at front and set it to None. Increment front as (front + 1) % length of array. Return the data at front.
+    c. Check if array is empty viz.  front == rear == -1. Raise an exception if so.
+
+Complexity:
+----------
+Time: O(2N) ~ O(N): We are traversing the array both ways front and rear. Since we ignore the constants, this has a linear time complexity.
+Space: O(N): Where N is the size of the queue.
+
+Advantages
+----------
+Helps to avoid wastage of space in queue.
+
+"""
 class CircularQueue:
     def __init__(self, initial_capacity=4):
         self._data = [None] * initial_capacity
